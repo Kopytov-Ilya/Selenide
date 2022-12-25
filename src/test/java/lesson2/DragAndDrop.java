2cde84f97d539f369d0ab32d197790f8b6c1562d
ILYA KOPYTOV <kopytov.ilya.rus@mail.ru>
2022-12-25 21:22:24 +0300

    Second lesson

---
+++ src/test/java/lesson2/DragAndDrop.java
@@ -0,0 +1,28 @@
+package lesson2;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDrop {
+    @Test
+    void SwappingRectangles() {
+        holdBrowserOpen = true;
+        //Откройте https://the-internet.herokuapp.com/drag_and_drop;
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //Перенесите прямоугольник А на место В;
+        $("#column-a").dragAndDropTo("#column-b");
+        //Проверьте, что прямоугольники действительно поменялись;
+        $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));
+
+      /*  open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform();
+        $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));*/
+
+    }
+}
